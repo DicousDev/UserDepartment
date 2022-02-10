777202a971c69f8c6d7f0f04479d7b86544feea1
João Victor Duarte <joaodicu387@gmail.com>
2022-02-10 02:05:42 -0300

    User class creation.

---
+++ src/main/java/com/userDepartament/entities/Department.java
@@ -0,0 +1,11 @@
+package com.userDepartament.entities;
+
+public class Department {
+	
+	private Long id = 0L;
+	private String name = "";
+	
+	public Department() {
+		
+	}
+}
---
+++ src/main/java/com/userDepartament/entities/User.java
@@ -0,0 +1,14 @@
+package com.userDepartament.entities;
+
+public class User {
+
+	private Long id = 0L;
+	private String name = "";
+	private String email = "";
+	
+	private Department department;
+	
+	public User() {
+		
+	}
+}
