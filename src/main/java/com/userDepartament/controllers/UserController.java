cf7c55f89a6096c92dabf4a1f079f26adf0891e0
João Victor Duarte <joaodicu387@gmail.com>
2022-02-11 02:41:04 -0300

    creating route to delete user.

--- src/main/java/com/userDepartament/controllers/UserController.java
+++ src/main/java/com/userDepartament/controllers/UserController.java
@@ -2,12 +2,16 @@ package com.userDepartament.controllers;
 
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.userDepartament.entities.User;
@@ -27,13 +31,36 @@ public class UserController {
 	}
 	
 	@GetMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
 	public User findById(@PathVariable Long id) {
 		User user = repository.findById(id).get();
 		return user;
 	}
 	
 	@PostMapping
-	public void insert(@RequestBody User user) {
+	public String insert(@RequestBody User user) {
 		repository.save(user);
+		JSONObject json = new JSONObject();
+		json.put("status", 201);
+		json.put("message", "User created successfully!");
+		return json.toString();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public String deleteById(@PathVariable("id") Long id) {
+		User user = repository.findById(id).get();
+		
+		JSONObject json = new JSONObject();
+		if(user == null) {
+			json.put("status", 401);
+			json.put("message", "user not found!");
+			return json.toString();
+		}
+
+		repository.deleteById(id);
+		json.put("status", 200);
+		json.put("message", "User successfully deleted!");
+		return json.toString();
+		
 	}
 }
